31e13263489c7594888aa73534bc38b08fa889d5
Ciaran Corrigan <ciaran_corrigan@hotmail.com>
2021-05-29 17:01:08 +0100

    Add project files.

---
+++ AmongThePines.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AmongThePines.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ AmongThePines.WebUI/Models/Products/Products.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmongThePines.WebUI.Models.Products
+{
+    public class Products
+    {
+    }
+}
